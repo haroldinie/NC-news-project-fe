805617b3432d0faff7c9bd29241baa2781fd8fd7
haroldinie <haroldinie123@gmail.com>
2024-05-18 13:03:42 +0100

    further styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import Home from './components/Home'
 import Articles from './components/Articles'
 import SingleArticleCard from './components/SingleArticleCard'
 import { Topics } from './components/Topics'
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Header() {
     return (
         <header id="header">
-            <h1>NorthCoders News Platform</h1>
+            <h1>NorthCoders News</h1>
             <main className="headerMain">
             <Link className="link" to="/articles">All Articles</Link>
             <Link className="link" to="/topics">Topics</Link>
